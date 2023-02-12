8211bffec2c1c172c7f767e78fb7cc8e978c863e
Alex D.V <91329795+DrunKnHigh@users.noreply.github.com>
2023-02-12 16:34:34 +0100

    fixed error merges

--- Assets/Dialogue/Script/NPCInteraction.cs
+++ Assets/Dialogue/Script/NPCInteraction.cs
@@ -4,18 +4,18 @@ using UnityEngine.UI;
 public class NPCInteraction : MonoBehaviour
 {
     public string message;
-    public TextMeshPro textMeshPro;
+    // public TextMeshPro textMeshPro;
 
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                textMeshPro = message;
-                //GUI.Label(new Rect(0, 0, 100, 100), message);
-            }
-        }
-    }
+    // private void OnTriggerStay(Collider other)
+    // {
+    //     if (other.gameObject.tag == "Player")
+    //     {
+    //         if (Input.GetKeyDown(KeyCode.E))
+    //         {
+    //             textMeshPro = message;
+    //             //GUI.Label(new Rect(0, 0, 100, 100), message);
+    //         }
+    //     }
+    // }
 }
 
